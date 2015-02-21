843b9f737395ddee9410b141ee142a6bae2c6538
George <georgecatalintudor@yahoo.com>
2015-02-21 15:03:11 +0200

    substances query

---
+++ app/scripts/controllers/substances.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name opendataApp.controller:SubstancesCtrl
+ * @description
+ * # SubstancesCtrl
+ * Controller of the opendataApp
+ */
+angular.module('opendataApp')
+  .controller('SubstancesCtrl', function ($scope, $http) {
+
+    $http.get("http://data.gov.ro/api/action/datastore_search?resource_id=a847b387-5f87-421d-97b0-8481f04d1359")
+    
+    .success(function(data, status, headers, config) {
+      $scope.meds = data.result.records;
+      console.log(data.result.records.den_produs);
+    })
+    .error(function(data, status, headers, config) {
+       $scope.errorMessage = "Couldn't load the list of meds, error # " + status;
+    });
+
+  });
