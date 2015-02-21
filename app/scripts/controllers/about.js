3332a2d562ceb045c5bdc5a4a9a97a0624577940
ethmelly <alexandru@melinescu.ro>
2015-02-21 12:02:05 +0200

    asd

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name opendataApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the opendataApp
+ */
+angular.module('opendataApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
