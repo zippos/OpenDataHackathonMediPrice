db11dde22d9a8a1e80b401e97b49c8cebf4ecc08
Anna <anna.dumitrache@gmail.com>
2015-02-21 23:40:22 +0200

    modal

--- app/scripts/controllers/substances.js
+++ app/scripts/controllers/substances.js
@@ -8,7 +8,7 @@
  * Controller of the opendataApp
  */
 angular.module('opendataApp')
-  .controller('SubstancesCtrl', function ($scope, $http) {
+  .controller('SubstancesCtrl', function ($scope, $http,$modal, $log) {
     $scope.priceList = [];
     $scope.total=0;
     $scope.update = function ( elem) {
@@ -85,5 +85,43 @@ angular.module('opendataApp')
       }
     }
 
+    $scope.items = ['item1', 'item2', 'item3'];
 
+    $scope.open = function (size) {
+
+      var modalInstance = $modal.open({
+        templateUrl: 'myModalContent.html',
+        controller: 'ModalInstanceCtrl',
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (selectedItem) {
+        $scope.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
+
+});
+
+angular.module('opendataApp').controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
 });
