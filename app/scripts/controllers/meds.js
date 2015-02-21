3f739ebfb4201ec7577c8b1deda0c1a2092f0715
iocty91 <iocty@yahoo.com>
2015-02-21 13:58:40 +0200

    controller melly

---
+++ app/scripts/controllers/meds.js
@@ -0,0 +1,11 @@
+/**
+ * Created by iacobstefan on 21/02/15.
+ */
+angular.module('opendataApp')
+  .controller('MedsCtrl', function ($scope) {
+    //$scope.awesomeThings = [
+    //  'HTML5 Boilerplate',
+    //  'AngularJS',
+    //  'Karma'
+    //];
+  });
