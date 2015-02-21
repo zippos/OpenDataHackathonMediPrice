58cb85e1359fa44a9fb1f6936b59c68ac1013238
bogdanmatra <bogdanmatra@gmail.com>
2015-02-21 13:51:36 +0200

    js

--- app/scripts/controllers/calculator.js
+++ app/scripts/controllers/calculator.js
@@ -1,3 +1,24 @@
-/**
- * Created by bogdanmatra on 21/02/15.
- */
+'use strict';
+
+angular.module('opendataApp')
+  .controller('CalculatorCtrl', function ($scope) {
+
+
+
+    var data = {
+      resource_id: 'a847b387-5f87-421d-97b0-8481f04d1359', // the resource id
+      limit: 5, // get 5 results
+      q: 'jones' // query for 'jones'
+    };
+    $.ajax({
+      url: 'http://data.gov.ro/api/action/datastore_search',
+      data: data,
+      dataType: 'jsonp',
+      success: function(data) {
+        alert('Total results found: ' + data.result.total)
+      }
+    });
+
+
+
+  });
