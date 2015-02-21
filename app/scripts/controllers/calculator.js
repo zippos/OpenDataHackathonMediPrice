661b91c700bdc23d0f518e11592f9a2906108d1f
bogdanmatra <bogdanmatra@gmail.com>
2015-02-21 15:35:37 +0200

    sql q

--- app/scripts/controllers/calculator.js
+++ app/scripts/controllers/calculator.js
@@ -9,7 +9,7 @@ angular.module('opendataApp')
         url:"http://data.gov.ro/api/action/datastore_search_sql",
         method:"GET",
         params:{
-          sql: "SELECT * from \"a847b387-5f87-421d-97b0-8481f04d1359\" WHERE den_produs LIKE '" + name +"%' ORDER BY pret_rid ASC LIMIT 1"
+          sql: "SELECT * from \"a847b387-5f87-421d-97b0-8481f04d1359\" WHERE den_produs LIKE '" + name +"%' ORDER BY pret_rid ASC LIMIT 3"
         }})
         .success(function(data){
           $scope.cheapest = data.result.records[0];
